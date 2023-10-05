9e22c2519253462b5d5d252ed80606adff2e8cee
phdljr <phdljr@naver.com>
2023-10-05 14:24:13 +0900

    feat: add firebase storage

--- js/db.js
+++ js/db.js
@@ -1,9 +1,11 @@
 // Firebase SDK 라이브러리 가져오기
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-firestore.js";
+import { getStorage } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-storage.js"
 import { firebaseConfig } from "./config.js"
 
 // Firebase 인스턴스 초기화
 const app = initializeApp(firebaseConfig);
 
 export const db = getFirestore(app);
+export const storage = getStorage(app);
