aa700e624b44a616ebabe2829cf7633c80616de6
phdljr <phdljr@naver.com>
2023-10-05 17:07:46 +0900

    feat: mapping firebase

--- js/db.js
+++ js/db.js
@@ -2,10 +2,12 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-firestore.js";
 import { getStorage } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-storage.js"
+// import { getAuth } from 'https://www.gstatic.com/firebasejs/9.22.0/firebase-auth.js'
 import { firebaseConfig } from "./config.js"
 
 // Firebase 인스턴스 초기화
 const app = initializeApp(firebaseConfig);
 
+// export const auth = getAuth(app);
 export const db = getFirestore(app);
 export const storage = getStorage(app);
